3448615871b075fa42bc239ba906c9d9d29f2d23
Chris Ardon <rockrisbl@gmail.com>
2022-01-01 13:04:16 -0600

    Check the letter

--- hangman.py
+++ hangman.py
@@ -7,3 +7,10 @@ print(chosen_word)
 
 #Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter: ").lower()
+
+#Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
