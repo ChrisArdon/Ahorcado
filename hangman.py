4300feec9aa85c2493c6ce7e3b956a27e3904e01
Chris Ardon <rockrisbl@gmail.com>
2022-01-01 13:32:28 -0600

    Create an empty List called display

--- hangman.py
+++ hangman.py
@@ -1,9 +1,18 @@
 import random
 
-word_list = ["aardvark", "baboon", "camel", "common", "element"]
+word_list = ["aardvark", "baboon", "camel", "common", "element", "andrea", "gisselle"]
 
 chosen_word = word_list[random.randint(0,len(word_list) - 1)]
-print(chosen_word)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create an empty List called display.
+display = []
+
+
+
+
 
 #Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter: ").lower()
