db29748b3fa0460780a75585286ce7d54e41e6d7
Chris Ardon <rockrisbl@gmail.com>
2022-01-01 13:01:30 -0600

    Ask the user to guess a letter

--- hangman.py
+++ hangman.py
@@ -4,3 +4,6 @@ word_list = ["aardvark", "baboon", "camel", "common", "element"]
 
 chosen_word = word_list[random.randint(0,len(word_list) - 1)]
 print(chosen_word)
+
+#Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ").lower()
